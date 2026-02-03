7acc958a7a2c77e34441b400e54650201801cfa1
JAIRO <al07076623@tecmilenio.mx>
2026-02-03 17:04:00 -0600

    carga 03/02/2026

---
+++ trabajos_nuevoprofe/prct2.js
@@ -0,0 +1,39 @@
+let a = 10; 
+let b = 3;
+console.log(a+b)
+console.log(a-b)
+console.log(a/b)
+console.log(a*b)
+console.log(a%b)
+console.log(a**b)
+let x= 5;
+x++;
+console.log(x)
+let y=9;
+y--;
+console.log(y)
+Z = 10 
+console.log(a+=b)
+console.log(a-=b)
+console.log(a*=b)
+console.log(a/=b)
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
+console.log(a==b)
+console.log(a===b)
+console.log(a!=b)
+console.log(a!==b)
+// ejercico 1 
+console.log(a+b);
+console.log(a%b)
+// ejerjcio 2 
+let usuario = " admin"
+let password = "0123"
+if (usuario === "admin" && password==="0123"){
+    console.log("acesso permitido")
+}
+// ejercicio 3 
+let edad =20 ;
+let mensaje = edad >= 18 
